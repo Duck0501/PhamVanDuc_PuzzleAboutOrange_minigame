7a8d747ece34541315adf8f5e9c772c53eb2097a
Duck0501 <vanduc050105@gmail.com>
2025-05-30 16:58:20 +0700

    done

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,12 +1,14 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     public Button buttonPlay;
+    public Button buttonHowToPlay;
     public Button buttonToHome;
     public Button buttonReset;
+    public Button buttonNext;
     public Button buttonLevel1;
     public Button buttonLevel2;
     public Button buttonLevel3;
@@ -18,6 +20,9 @@ public class UIManager : MonoBehaviour
     {
         if (buttonPlay != null)
             buttonPlay.onClick.AddListener(() => SceneManager.LoadScene("Level"));
+
+        if (buttonHowToPlay != null)
+            buttonHowToPlay.onClick.AddListener(() => SceneManager.LoadScene("How to play"));
 
         if (buttonToHome != null)
             buttonToHome.onClick.AddListener(() => SceneManager.LoadScene("Home"));
@@ -41,6 +46,54 @@ public class UIManager : MonoBehaviour
             buttonLevel6.onClick.AddListener(() => SceneManager.LoadScene("Level 6"));
 
         if (buttonReset != null)
-            buttonReset.onClick.AddListener(() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex));
+            buttonReset.onClick.AddListener(() =>
+            {
+                if (SceneManager.GetActiveScene().name == "Win" || SceneManager.GetActiveScene().name == "Lose")
+                {
+                    string lastLevel = PlayerPrefs.GetString("LastLevel", "Level 1");
+                    SceneManager.LoadScene(lastLevel);
+                }
+                else
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                }
+            });
+
+        if (buttonNext != null)
+            buttonNext.onClick.AddListener(() =>
+            {
+                if (SceneManager.GetActiveScene().name == "Win")
+                {
+                    string lastLevel = PlayerPrefs.GetString("LastLevel", "Level 1");
+                    string nextLevel;
+
+                    switch (lastLevel)
+                    {
+                        case "Level 1":
+                            nextLevel = "Level 2";
+                            break;
+                        case "Level 2":
+                            nextLevel = "Level 3";
+                            break;
+                        case "Level 3":
+                            nextLevel = "Level 4";
+                            break;
+                        case "Level 4":
+                            nextLevel = "Level 5";
+                            break;
+                        case "Level 5":
+                            nextLevel = "Level 6";
+                            break;
+                        case "Level 6":
+                            nextLevel = "Home";
+                            break;
+                        default:
+                            nextLevel = "Level 1";
+                            break;
+                    }
+
+                    SceneManager.LoadScene(nextLevel);
+                }
+            });
     }
 }
